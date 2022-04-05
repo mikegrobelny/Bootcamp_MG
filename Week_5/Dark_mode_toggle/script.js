36b606bd7bfb2cfffcf4c5bff38a03d5a9691640
mikegrobelny <95480298+mikegrobelny@users.noreply.github.com>
2022-04-05 07:07:16 +0100

    Week 5

---
+++ Week_5/Dark_mode_toggle/script.js
@@ -0,0 +1,4 @@
+function darkMode() {
+    document.querySelector(".darkMode").classList.toggle("darkMode-active") +
+      document.querySelector(".whiteMode").classList.toggle("whiteMode-active");
+  }
